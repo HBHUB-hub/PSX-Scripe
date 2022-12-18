af941e01cdc1b98b02809bd9c03f2a3299752c41
HB_HUB <111684273+HBHUB-hub@users.noreply.github.com>
2022-12-18 12:21:15 +0800

    Update PSX-TRADE-SCAM-Local.lua

--- PSX-TRADE-SCAM-Local.lua
+++ PSX-TRADE-SCAM-Local.lua
@@ -1,1 +1,6 @@
-
+Uncaught exception:
+[string "bundle:deps/require.lua"]:301: /home/runner/WORKIN-OBF/obfuscated/HB_HUB#0413.lua:245: '<eof>' expected near 'end'
+stack traceback:
+	[C]: in function 'assert'
+	[string "bundle:deps/require.lua"]:301: in function 'require'
+	[string "bundle:/main.lua"]:128: in function <[string "bundle:/main.lua"]:20>
